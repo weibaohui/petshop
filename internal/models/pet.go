ca40b729261e48f9781134ded821a09b9d4fbfeb
weibaohui <weibaohui@yeah.net>
2026-04-01 12:38:57 +0800

    feat: 完成需求 mSvKQBhHCkxLy4Qxmpupq

--- internal/models/pet.go
+++ internal/models/pet.go
@@ -1,9 +1,9 @@
 package models
 
 type Pet struct {
-	ID       int64    `json:"id"`
-	Name     string   `json:"name"`
-	Type     string   `json:"type"`
+	ID        int64    `json:"id"`
+	Name      string   `json:"name"`
+	Type      string   `json:"type"`
 	PhotoUrls []string `json:"photoUrls"`
-	Status   string   `json:"status"`
+	Status    string   `json:"status"`
 }
